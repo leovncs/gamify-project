832c087c4a8e6366271509dc5ebc033a2f188082
leovncs <leoovncs@gmail.com>
2024-11-11 16:34:35 -0300

    Initial Commit

---
+++ src/test/java/com/leovncs/gamify/GamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leovncs.gamify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
